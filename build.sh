194b7dbefced47864477819be84b7c69c77f5f25
bjornregnell <bjorn.regnell@cs.lth.se>
2025-02-06 14:01:55 +0100

    shorten title

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 source src/krav/build.sh
 source src/pgk/build.sh
-pandoc -s --toc -c pandoc.css -B top.html --metadata title="Lund University Open Source Web Pages" src/index.md -o index.html
+pandoc -s --toc -c pandoc.css -B top.html --metadata title="Lund University Open Source Pages" src/index.md -o index.html
