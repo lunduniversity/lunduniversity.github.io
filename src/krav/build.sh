eb8e05d7286197a27c94f92a8b82cbf2b58575d4
bjornregnell <bjorn.regnell@cs.lth.se>
2025-05-08 11:42:47 +0200

    rename css file

--- src/krav/build.sh
+++ src/krav/build.sh
@@ -1,2 +1,2 @@
 mkdir -p krav
-pandoc -s --toc -c ../pandoc.css -B top.html --metadata title="ETSN15 Software Requirements Engineering" src/krav/index.md -o krav/index.html
+pandoc -s --toc -c ../style.css -B top.html --metadata title="ETSN15 Software Requirements Engineering" src/krav/index.md -o krav/index.html
--- src/pgk/build.sh
+++ src/pgk/build.sh
@@ -1,2 +1,2 @@
 mkdir -p pgk
-pandoc -s --toc -c ../pandoc.css -B top.html --metadata title="EDAB05 Introduktion till programmering" src/pgk/index.md -o pgk/index.html
+pandoc -s --toc -c ../style.css -B top.html --metadata title="EDAB05 Introduktion till programmering" src/pgk/index.md -o pgk/index.html
