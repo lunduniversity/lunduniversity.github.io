9c6a1a3e4369a6bbc6871bfda63cee151fbd9ee9
bjornregnell <bjorn.regnell@cs.lth.se>
2025-05-08 17:05:06 +0200

    fix heading of pgk

--- src/pgk/build.sh
+++ src/pgk/build.sh
@@ -1,2 +1,2 @@
 mkdir -p pgk
-pandoc -s --toc -c ../style.css -B top.html --metadata title="EDAB05 Introduktion till programmering" src/pgk/index.md -o pgk/index.html
+pandoc -s --toc -c ../style.css -B top.html --metadata title="EDAB05 Programmering, grundkurs" src/pgk/index.md -o pgk/index.html
